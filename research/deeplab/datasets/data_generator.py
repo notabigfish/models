92d64c120af27c93c6039407679f9230ef837505
notabigfish <33329245+notabigfish@users.noreply.github.com>
2020-01-05 11:35:51 +0800

    Update data_generator.py

--- research/deeplab/datasets/data_generator.py
+++ research/deeplab/datasets/data_generator.py
@@ -99,10 +99,20 @@ _ADE20K_INFORMATION = DatasetDescriptor(
     ignore_label=0,
 )
 
+_ADE20K_ZS_INFORMATION = DatasetDescriptor(
+    splits_to_sizes={
+        'train': 11193,  # num of samples in images/training
+        'val': 1075,  # num of samples in images/validation
+    },
+    num_classes=2,
+    ignore_label=255,
+)
+
 _DATASETS_INFORMATION = {
     'cityscapes': _CITYSCAPES_INFORMATION,
     'pascal_voc_seg': _PASCAL_VOC_SEG_INFORMATION,
     'ade20k': _ADE20K_INFORMATION,
+    'ade20k_zs': _ADE20K_ZS_INFORMATION,
 }
 
 # Default file pattern of TFRecord of TensorFlow Example.
